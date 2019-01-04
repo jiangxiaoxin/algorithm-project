78e900a2a287fd6e2671e8c579af72737ee5b1b8
jiangxiaoxin <jiangyanxin1989@gmail.com>
2019-01-04 11:18:45 +0800

    插入排序测试

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { test } from "./find_minimal_number_in_sorted_array";
+// import { test } from "./find_minimal_number_in_sorted_array";
+import { test } from "./sort/insertion_sort";
 
-
-test() //  find_minimal_number_in_sorted_array
+test()
