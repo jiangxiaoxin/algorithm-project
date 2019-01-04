e588a287be82bd183a4c270d0e897e5d1d50e6f4
jiangxiaoxin <jiangyanxin1989@gmail.com>
2019-01-04 16:05:50 +0800

    update：添加测试

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 // import { test } from "./find_minimal_number_in_sorted_array";
-import { test } from "./sort/insertion_sort";
+// import { test } from "./sort/insertion_sort";
+import { test } from "./sort/merge_sort";
 
 test()
+
