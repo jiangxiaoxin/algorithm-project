27b380988253f5ffde1f21962c0543a87c4468ce
jiangxiaoxin <jiangyanxin1989@gmail.com>
2019-11-12 10:38:29 +0800

    更改结构

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,4 @@
-// import { test } from "./find_minimal_number_in_sorted_array";
-// import { test } from "./sort/insertion_sort";
-// import { test } from "./sort/merge_sort";
-// import { test } from "./find_maximum_subarray";
-// import { test } from "./find_maximum_subarray/index2";
-// import { test } from "./sort/heap_sort"
-// import { test } from './sort/quick_sort'
-// import { test } from './data_struc/binary_tree/index'
+
 
 
 
